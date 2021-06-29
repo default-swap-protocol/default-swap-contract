1ca7834acdd468dbb666cd8c777a54f0e96c7993
Tai <4382091+taisukemino@users.noreply.github.com>
2021-06-29 13:19:57 +0900

    renaming for clarity

--- contracts/PremToken.sol
+++ contracts/PremToken.sol
@@ -3,6 +3,6 @@ pragma solidity 0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract pToken is ERC20 {
+contract PremToken is ERC20 {
   constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
 }
