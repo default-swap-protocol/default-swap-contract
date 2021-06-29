4d906a0838facb0a292663d0bbb0ab3681062e38
Tai <4382091+taisukemino@users.noreply.github.com>
2021-06-29 09:31:44 +0900

    a premium token contract

---
+++ contracts/pToken.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract pToken is ERC20 {
+  constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+}
